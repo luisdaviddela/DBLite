d5066b44fe3065ef034e5e1864620e34f6a1d661
David De La Cruz <delacruz_luis@outlook.es>
2019-08-20 12:36:28 -0500

    Delete Async

--- DBExample/DBExample/DBExample/ViewModels/DeleteViewModel.cs
+++ DBExample/DBExample/DBExample/ViewModels/DeleteViewModel.cs
@@ -1,39 +1,37 @@
 ﻿using ReactiveUI;
+using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 namespace DBExample
 {
     internal class DeleteViewModel:ReactiveObject, INotifyPropertyChanged
     {
         ReactiveList<PersonModel> _todos;
         private PersonModel selectedCategories;
+        public ReactiveCommand DeleteCommand { get; private set; }
         public ReactiveList<PersonModel> Todos
         {
             get => _todos;
             set => this.RaiseAndSetIfChanged(ref _todos, value);
         }
-        private PersonModel _selectedTodo;
-        public PersonModel SelectedTodo
-        {
-            get => _selectedTodo;
-            set => this.RaiseAndSetIfChanged(ref _selectedTodo, value);
-        }
-
+      
         PersonLiteDBService _liteDBService;
         public DeleteViewModel()
         {
             _liteDBService = new PersonLiteDBService();
             var todos = _liteDBService.ReadAllItems();
-            //var sg = todos.Count();
-            //Application.Current.MainPage.DisplayAlert("dsa", sg.ToString(), "da");
+            
             if (todos.Any())
             {
                 Todos = new ReactiveList<PersonModel>(todos) { ChangeTrackingEnabled = true };
             }
             else { Todos = new ReactiveList<PersonModel>() { ChangeTrackingEnabled = true }; }
-        }
-        PersonModel _yourSelectedItem;
 
+            
+        }
+       
         public event PropertyChangedEventHandler PropertyChanged;
 
         public PersonModel SelectedCategories
@@ -43,13 +41,37 @@ namespace DBExample
             {
                 if (selectedCategories != value)
                 {
+                    SetProperty(ref selectedCategories, value);
                     OnTapSelectedCategories();
                 }
             }
         }
+
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(field, value)) { return false; }
+
+            field = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         private async void OnTapSelectedCategories()
         {
-           
+            try
+            {
+                
+                var todo = new PersonModel() { Id = SelectedCategories.Id, Nombre = SelectedCategories.Nombre, Edad = SelectedCategories.Edad };
+                _liteDBService.DeleteItemAsync(todo);
+                await Application.Current.MainPage.DisplayAlert("LiteDB Message", "Eliminado correctamente", "Ok");
+            }
+            catch (System.Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("LiteDB Error",ex.Message,"Ok");
+            }
         }
     }
 }
